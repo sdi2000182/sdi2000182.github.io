6e03e00199ccb5f6cf5b7c9089b8e6693fcdbb82
sdi2000182 <sdi2000182@di.uoa.gr>
2024-05-29 01:50:11 +0300

    removed logo

---
+++ my-app/src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+    return (
+        <div>
+            <h1>Hello, World!</h1>
+            <p>This is Theodora.</p>
+        </div>
+    );
+}
+
+export default MyComponent;
